67cac6e4a30d4e90dfd7fca8c8d4b2a731946d5d
Matheus Silva <88209334+matheussilvacydevs@users.noreply.github.com>
2024-06-14 16:07:28 -0400

    Add files via upload

--- portifolio/meuportifolio/components/main/ProjetosDev.tsx
+++ portifolio/meuportifolio/components/main/ProjetosDev.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 import { motion } from "framer-motion";
-import { slideInFromTop } from "@/utils/motion";
+import { slideInFromTop } from "../../utils/motion";
 import Image from "next/image";
 import ProjectCard from "../sub/ProjectCard";
 
